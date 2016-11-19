cd7e4c5f18aa6551a12b622ac0c098dbb5effa07
Nikita <kobel169@gmail.com>
2016-11-20 01:32:22 +0300

    Dirty hack for local usage

--- recipes/osd.rb
+++ recipes/osd.rb
@@ -172,8 +172,9 @@ if node['ceph']['osd']['devices']
     # is_ceph - Does the device contain the default 'ceph data' or 'ceph journal' label
     # The -v option is added to the ceph-disk script so as to get a verbose output if debugging is needed. No other reason.
     # is_ceph=$(parted --script #{osd_device['data']} print | egrep -sq '^ 1.*ceph')
+    id=#{node.name[-1].to_i - 1}}
+
     execute "ceph-disk-prepare on #{osd_device['data']}" do
-      id=#{node.name[-1].to_i - 1}}
       command <<-EOH
         is_device=$(echo '#{osd_device['data']}' | egrep '/dev/(([a-z]{3,4}[0-9]$)|(cciss/c[0-9]{1}d[0-9]{1}p[0-9]$))')
         ceph-disk -v prepare --cluster #{node['ceph']['cluster']} #{dmcrypt} --fs-type #{node['ceph']['osd']['fs_type']} #{osd_device['data']} #{osd_device['journal']}
